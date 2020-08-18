f31a6555dad463a36b2c2c799745c420c4920afe
owenchang1992 <>
2020-08-18 14:45:12 +0800

    Add analyse function

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = (env={}, argv={}) => ({
     module: {
         rules: require('./module.rules')(env, argv)
     },
     plugins: [
+        env.analyse ? new BundleAnalyzerPlugin() : null,  
         argv.mode === 'development' ? new HtmlWebpackPlugin : null,
         argv.mode === "production"
             ? new MiniCssExtractPlugin({
                 filename: "[name].css",
                 chunkFilename: "[id].css"
-            }) : null
+            }) : null  
     ].filter(
-        plugin => !!plugin
+        plugin => {
+            console.log(plugin)
+            return plugin !== null
+        }
     ),
     devtool: 'source-map'
 })
