4974b402c4a71cc2bc87cf2ceef6f05815d5d6f7
owenchang1992 <>
2020-08-18 11:35:44 +0800

    Clean the code

---
+++ module.rules.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = (env, argv) => [
+    {
+        test: /\.js$/,
+        use: 'babel-loader'
+    },
+    {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+            'file-loader',
+            {
+                loader: 'image-webpack-loader',
+                options: {
+                    disable: true
+                }
+            }
+        ]
+    },
+    {
+        test: /\.(ogg|mp3|wav|mpe?g)/i,
+        use: 'file-loader'
+    },
+    {
+        test: /\.scss$/,
+        use: [
+            argv.mode === "production"
+                ? MiniCssExtractPlugin.loader
+                : 'style-loader', 
+            {
+                loader: 'css-loader', options: {
+                    sourceMap: true
+                }
+            },
+            {
+                loader: 'sass-loader', options: {
+                    sourceMap: true
+                }
+            } 
+        ]
+    }
+]
--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env={}, argv={}) => ({
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            disable: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(ogg|mp3|wav|mpe?g)/i,
-                use: 'file-loader'
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    argv.mode === "production"
-                        ? MiniCssExtractPlugin.loader
-                        : 'style-loader', 
-                    {
-                        loader: 'css-loader', options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader', options: {
-                            sourceMap: true
-                        }
-                    } 
-                ]
-            }
-        ]
+        rules: [ require('./module.rules')(env, argv) ]
     },
     plugins: [
         argv.mode === 'development' ? new HtmlWebpackPlugin : null,
