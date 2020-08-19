60642bea4756dcdbd58b268c23990ce72679710d
owenchang1992 <>
2020-08-19 23:05:43 +0800

    Add react to the project

--- module.rules.js
+++ module.rules.js
@@ -2,7 +2,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = (env, argv) => [
     {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         use: 'babel-loader'
     },
     {
