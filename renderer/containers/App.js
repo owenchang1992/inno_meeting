1de2d3076834630fe22e7da70ad8153e8384397d
owenchang1992 <>
2020-08-25 10:40:17 +0800

    Move App to container folder

--- renderer/containers/App.js
+++ renderer/containers/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Link, HashRouter } from 'react-router-dom';
 
-import Page1 from './page1';
-import Page2 from './page2';
+import Page1 from '../components/page1';
+import Page2 from '../components/page2';
 
 const App = () => (
   <HashRouter>
     <>
-      <Route path="/" exact component={() => <h1>Home Page</h1>} />
+      <Route path="/" exact component={() => <h1>Home Page asb</h1>} />
       <Route path="/page1" component={Page1} />
       <Route path="/page2" component={Page2} />
     </>
