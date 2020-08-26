9867b7cdfe14b92a09dd974a2b36e9a172df970f
owenchang1992 <>
2020-08-26 16:32:57 +0800

    Setting header

--- renderer/containers/App.js
+++ renderer/containers/App.js
@@ -3,11 +3,13 @@ import { HashRouter as Router } from 'react-router-dom';
 import '../sources/css/photon.css';
 
 import Main from './main_pane';
+import Header from './header';
 import SideBar from './sidebar';
 
 const App = () => (
   <Router>
     <div className="window">
+      <Header />
       <div className="window-content">
         <div className="pane-group">
           <SideBar />
---
+++ renderer/containers/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="toolbar toolbar-header">
+    <h1 className="title">media manager</h1>
+  </div>
+);
+
+export default Header;
