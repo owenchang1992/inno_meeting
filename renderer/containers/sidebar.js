264b13c7f2f1930e002f33c3818793babaf8d074
owenchang1992 <>
2020-08-27 13:14:20 +0800

    Fix the loading page error

--- renderer/containers/sidebar.js
+++ renderer/containers/sidebar.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-const SideBar = () => (
-  <div className="pane-sm sidebar">
-    <nav className="nav-group">
-      <h5 className="nav-group-title">Pages</h5>
-      {[1, 2].map((number) => (
-        <span className="nav-group-item">
-          <span className="icon icon-doc-text-inv" />
-          <Link to={`/page${number}`} style={{ textDecoration: 'none', color: '#333' }}>
+const SideBar = () => {
+  const history = useHistory();
+
+  const handleClick = (path) => {
+    history.push(path);
+  };
+
+  return (
+    <div className="pane-sm sidebar">
+      <nav className="nav-group">
+        <h5 className="nav-group-title">Pages</h5>
+        {[1, 2].map((number) => (
+          <span
+            role="button"
+            onClick={() => handleClick(`Page${number}`)}
+            onKeyDown={() => { console.log('key down'); }}
+            tabIndex={0}
+            className="nav-group-item"
+          >
+            <span className="icon icon-doc-text-inv" />
             {`Page ${number}`}
-          </Link>
-        </span>
-      ))}
-    </nav>
-  </div>
-);
+          </span>
+        ))}
+      </nav>
+    </div>
+  );
+};
 
 export default SideBar;
