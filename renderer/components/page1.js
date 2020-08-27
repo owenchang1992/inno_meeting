92abb87fc44038c098f35b101bc3f768d64383a1
owenchang1992 <>
2020-08-27 16:15:33 +0800

    test

--- renderer/components/page1.js
+++ renderer/components/page1.js
@@ -1,21 +1,28 @@
-import React, { useEffect } from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Grid from '@material-ui/core/Grid';
-
-// const useStyles = makeStyles(() => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-// }));
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function imageViewer() {
-  // const classes = useStyles();
+  const canvasRef = useRef(null);
+  const [canvasImg, setcanvasImg] = useState(null)
+
+  const getImg = (src) => {
+    const img = new Image();
+    img.src = src;
+
+    return img;
+  };
 
   useEffect(() => {
-    // console.log(prevProps);
-  });
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    context.fillStyle = '#000000';
+    const img = getImg('67B70A8E-C389-4660-BEC3-8C39E8082287_1_105_c.jpeg');
+    img.onload = () => {
+      context.drawImage(img, 0, 0, img.naturalWidth, img.naturalheight);
+    };
+  }, []);
 
   return (
-    <div>Page 1</div>
+    <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />
   );
 }
