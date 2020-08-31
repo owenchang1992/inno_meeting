cdc488fff7218e127f4677e044e66b80bc0aab07
owenchang1992 <>
2020-08-31 13:39:19 +0800

    Add Drawing function

--- renderer/components/page1.js
+++ renderer/components/page1.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-export default function imageViewer() {
+export default function imageViewer({ imagePath }) {
   const canvasRef = useRef(null);
   const dpi = window.devicePixelRatio;
-  const [canvas]
 
   const loadImage = (src) => new Promise((resolve, reject) => {
     const img = new Image();
@@ -17,46 +16,35 @@ export default function imageViewer() {
       clearTimeout(timeoutTimer);
     };
   });
-  
-  const drawImage = (path, canvas, ctx) => {
-    return loadImage(path)
-      .then((img) => {
-        const width = img.naturalWidth * dpi;
-        const height = img.naturalHeight * dpi;
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        ctx.drawImage(img, 0, 0, width, height);
-        return 
-      })
-      .catch((err) => console.log('loading image error'))
-  };
 
-  const drawＲectangle = (ctx) => {
+  const drawImage = (path, canvas, ctx) => loadImage(path)
+    .then((img) => {
+      const width = img.naturalWidth * dpi;
+      const height = img.naturalHeight * dpi;
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      ctx.drawImage(img, 0, 0, width, height);
+    })
+    .catch(() => console.log('loading image error'));
+
+  const drawRectangle = (ctx) => {
     ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.rect(20, 20, 150, 100);
     ctx.stroke();
   };
 
-  const reDraw = () => {
-
-  };
-
-  const loadConvas = () => {
+  useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
-    return 
-  }
 
-  useEffect(() => {
-    drawＲectangle
-  });
-
-  drawImage('67B70A8E-C389-4660-BEC3-8C39E8082287_1_105_c.jpeg', )
+    drawImage(imagePath, canvas, context)
+      .then(() => drawRectangle(context));
+  }, [imagePath]);
 
   return (
     <div id="canvas-container" style={{ width: '100%', height: '100%' }}>
-      <canvas ref={canvasRef} style={{width: "100%"}}/>
+      <canvas ref={canvasRef} style={{ width: '100%' }} />
     </div>
   );
 }
--- renderer/containers/main_pane.js
+++ renderer/containers/main_pane.js
@@ -8,7 +8,7 @@ const Main = () => (
   <div className="pane">
     <>
       <Route path="/" exact component={() => <h1>Home Page asb</h1>} />
-      <Route path="/page1" component={Page1} />
+      <Route path="/page1" component={() => <Page1 imagePath="67B70A8E-C389-4660-BEC3-8C39E8082287_1_105_c.jpeg" />} />
       <Route path="/page2" component={Page2} />
     </>
   </div>
