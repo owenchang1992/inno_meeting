a4ff6e5f87dfa56cc3ba237db71f7b047ed488eb
owenchang1992 <>
2020-08-31 18:20:46 +0800

    Use tab to control contents of the main pane

--- renderer/components/tabs.js
+++ renderer/components/tabs.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function tabs({ pages }) {
+  const history = useHistory();
+
+  const handleClick = (path) => {
+    history.push(path);
+  };
+
   return (
     <div className="tab-group">
       {
         pages.map((page) => (
-          <div className="tab-item">
+          <div
+            className="tab-item"
+            role="button"
+            onClick={() => handleClick(page.routingPath)}
+            onKeyDown={() => { console.log('key down'); }}
+            tabIndex={0}
+          >
             <span className="icon icon-cancel icon-close-tab" />
             {page.name}
           </div>
