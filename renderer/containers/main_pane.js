4c59d0f8d1f3aae376afaef473830a2cefb1585d
owenchang1992 <>
2020-09-02 10:50:48 +0800

    Extract pages to 'pages' folder

--- renderer/containers/main_pane.js
+++ renderer/containers/main_pane.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import ImageEditor from '../components/image_editor/image_editor';
-import Page2 from '../components/page2';
+import ImageEditor from '../components/pages/image_editor/image_editor';
+// import Page2 from '../components/page2';
 import Tabs from '../components/tabs';
 
 const main = ({ pages }) => (
@@ -10,15 +10,18 @@ const main = ({ pages }) => (
     <Tabs pages={pages} />
     <>
       <Route path="/" exact component={() => <h1>Home Page asb</h1>} />
-      <Route
-        path="/page1"
-        component={() => (
-          <ImageEditor
-            imagePath="67B70A8E-C389-4660-BEC3-8C39E8082287_1_105_c.jpeg"
+      {
+        pages.map((page) => (
+          <Route
+            path={page.routingPath}
+            component={() => (
+              <ImageEditor
+                imagePath="67B70A8E-C389-4660-BEC3-8C39E8082287_1_105_c.jpeg"
+              />
+            )}
           />
-        )}
-      />
-      <Route path="/page2" component={Page2} />
+        ))
+      }
     </>
   </div>
 );
