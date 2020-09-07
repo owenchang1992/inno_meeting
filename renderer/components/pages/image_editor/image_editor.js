1d30ac1b7d7ffabd1502393780052bde30f81742
owenchang1992 <>
2020-09-07 15:09:29 +0800

    Ｍodify the layout

--- renderer/components/pages/image_editor/image_editor.js
+++ renderer/components/pages/image_editor/image_editor.js
@@ -1,15 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { loadImage, drawRectangle } from './editor_utils';
 
+const baseStyle = {
+  borderRadius: '4px',
+  boxShadow: '0px 1px 3px 0px rgba(0, 0, 0, 0.12)',
+};
+
 export default function imageEditor({ imagePath }) {
   const canvasRef = useRef(null);
   const [canvasStyle, setCanvasStyle] = useState({ width: '100%' });
+  const [mouseDownPoint] = useState({ left: 23, top: 56 });
   const dpi = window.devicePixelRatio;
 
   const drawImage = (path, canvas, ctx) => loadImage(path)
     .then((img) => {
-      if (img.naturalWidth < img.naturalHeight + 25) setCanvasStyle({ height: 'calc(100% - 25px)' });
-      else setCanvasStyle({ width: '100%' });
+      if (img.naturalWidth < img.naturalHeight + 25) {
+        setCanvasStyle({
+          ...baseStyle,
+          height: 'calc(100%)',
+        });
+      } else {
+        setCanvasStyle({
+          ...baseStyle,
+          width: '100%',
+        });
+      }
       const width = img.naturalWidth * dpi;
       const height = img.naturalHeight * dpi;
       canvas.setAttribute('width', width);
@@ -37,10 +52,11 @@ export default function imageEditor({ imagePath }) {
       id="canvas-container"
       style={{
         width: '100%',
-        height: '100%',
+        height: 'calc(100% - 25px)',
         display: 'flex',
         justifyContent: 'center',
         alignContent: 'center',
+        padding: '5px',
       }}
     >
       <canvas ref={canvasRef} style={canvasStyle} />
