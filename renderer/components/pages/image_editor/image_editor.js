aa9158c67ff6e4a649a006334bf1b8af74199386
owenchang1992 <>
2020-09-07 23:30:52 +0800

    Fix the loading handler process

--- renderer/components/pages/image_editor/image_editor.js
+++ renderer/components/pages/image_editor/image_editor.js
@@ -8,71 +8,71 @@ const baseStyle = {
 
 export default function imageEditor({ imagePath }) {
   const canvasRef = useRef(null);
-  const [content, setContent] = useState(null);
-  const [canvasStyle, setCanvasStyle] = useState({ width: '100%' });
+  const [content, setContent] = useState(<div>loading</div>);
   const [mouseDownPoint, setMouseDownPoint] = useState({ left: -1, top: -1 });
   const dpi = window.devicePixelRatio;
   console.log('iamge page');
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    // setContent(<div>Loading</div>);
-    const drawImage = (path, imgCanvas, ctx) => loadImage(path)
+    const onMouseDown = (e) => {
+      if (e.type === 'mousedown') {
+        setMouseDownPoint({
+          left: e.nativeEvent.offsetX,
+          top: e.nativeEvent.offsetY,
+        });
+      }
+    };
+
+    const drawImage = (path) => loadImage(path)
       .then((img) => {
-        if (img.naturalWidth < img.naturalHeight + 25) {
-          setCanvasStyle({
-            ...baseStyle,
-            height: 'calc(100%)',
-          });
-        } else {
-          setCanvasStyle({
-            ...baseStyle,
-            width: '100%',
-          });
-        }
         const width = img.naturalWidth * dpi;
         const height = img.naturalHeight * dpi;
-        imgCanvas.setAttribute('width', width);
-        imgCanvas.setAttribute('height', height);
-        ctx.drawImage(img, 0, 0, width, height);
+        setContent(
+          <canvas
+            ref={canvasRef}
+            width={width}
+            height={height}
+            style={
+              img.naturalWidth < img.naturalHeight + 25
+                ? { ...baseStyle, height: 'calc(100%)' }
+                : { ...baseStyle, width: '100%' }
+            }
+            onMouseDown={(e) => onMouseDown(e)}
+          />,
+        );
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        context.drawImage(img, 0, 0, width, height);
       })
       .catch(() => {
         console.log('loading image error');
         setContent(<div>Loading Media Error</div>);
       });
 
-    drawImage(imagePath, canvas, context);
-  }, [imagePath]);
+    drawImage(imagePath);
+  }, []);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
+    if (content.type === 'canvas') {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
 
-    const scale = () => ({
-      scaleX: canvas.width / context.canvas.offsetWidth,
-      scaleY: canvas.height / context.canvas.offsetHeight,
-    });
+      const scale = () => ({
+        scaleX: canvas.width / context.canvas.offsetWidth,
+        scaleY: canvas.height / context.canvas.offsetHeight,
+      });
 
-    if (mouseDownPoint.left !== -1 && mouseDownPoint.top !== -1 && content === null) {
-      drawRectangle({
-        left: mouseDownPoint.left * scale().scaleX,
-        top: mouseDownPoint.top * scale().scaleY,
-        width: 400,
-        height: 300,
-        color: 'red',
-      }, canvas, context);
+      if (mouseDownPoint.left !== -1 && mouseDownPoint.top !== -1 && content !== null) {
+        drawRectangle({
+          left: mouseDownPoint.left * scale().scaleX,
+          top: mouseDownPoint.top * scale().scaleY,
+          width: 400,
+          height: 300,
+          color: 'red',
+        }, canvas, context);
+      }
     }
   }, [mouseDownPoint]);
-
-  const onMouseDown = (e) => {
-    if (e.type === 'mousedown') {
-      setMouseDownPoint({
-        left: e.nativeEvent.offsetX,
-        top: e.nativeEvent.offsetY,
-      });
-    }
-  };
 
   return (
     <div
@@ -86,15 +86,7 @@ export default function imageEditor({ imagePath }) {
         padding: '5px',
       }}
     >
-      {
-        content !== null ? content : (
-          <canvas
-            ref={canvasRef}
-            style={canvasStyle}
-            onMouseDown={(e) => onMouseDown(e)}
-          />
-        )
-      }
+      { content }
     </div>
   );
 }
