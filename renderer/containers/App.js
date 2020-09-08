970c2f628a23ad8f08c698f387bfdfb392300742
owenchang1992 <>
2020-09-08 14:11:28 +0800

    Clean code

--- renderer/containers/App.js
+++ renderer/containers/App.js
@@ -6,25 +6,6 @@ import Main from './main_pane';
 import Header from './header';
 import SideBar from './sidebar';
 
-/**
- * name: the name for listing at sideBar
- * type:
- */
-
-// let counter = 1;
-
-// const createNewPage = () => {
-//   counter += 1;
-//   return ({
-//     name: `Page ${counter}`,
-//     type: 'image_editor',
-//     routingPath: `/page${counter}`,
-//     props: {
-//       imagePath: '67B70A8E-C389-4660-BEC3-8C39E8082287_1_105_c.jpeg',
-//     },
-//   });
-// };
-
 const page1 = {
   name: 'page 1',
   type: 'image_editor',
