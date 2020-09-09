dfc01147e2d776957b2d0d48e385cdd638b0e73d
owenchang1992 <>
2020-09-09 11:28:58 +0800

    Extract the findIndex method

--- renderer/reducers/page_reducer.js
+++ renderer/reducers/page_reducer.js
@@ -1,9 +1,11 @@
 import { ADD_PAGE, CLOSE_PAGE, UPDATE_PAGE_PROPERTIES } from './constants';
 
+const findPageIndex = (pageList, foundPage) => (
+  pageList.findIndex((page) => foundPage.routingPath === page.routingPath)
+);
+
 const removePageFromState = (pages, removedPage) => {
-  const rmPageIndex = pages.findIndex(
-    (page) => removedPage.routingPath === page.routingPath,
-  );
+  const rmPageIndex = findPageIndex(pages, removedPage);
   pages.splice(rmPageIndex, 1);
   return [...pages];
 };
