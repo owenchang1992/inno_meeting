2a1ee4ef21fed9380fda63186a4cb299450835c3
owenchang1992 <>
2020-09-14 18:24:07 +0800

    Show history list

--- renderer/components/pages/image_editor/labels.js
+++ renderer/components/pages/image_editor/labels.js
@@ -6,12 +6,24 @@ export default ({ history }) => {
   return (
     <div>
       <h5 className="nav-group-title">Events</h5>
-      <div className="list-group-item" style={{ padding: '5px 10px', border: '1px solid #ddd', borderRadius: '3px' }}>
-        <span className="icon icon-record" style={{ color: '#fdbc40', marginRight: '5px' }} />
-        <strong>{history.length <= 1 ? 'fdf' : history[1].action}</strong>
-        <br />
-        Lorem ipsum dolor sit amet.
-      </div>
+      {
+        history.map((value) => (
+          <div
+            className="list-group-item"
+            style={{
+              padding: '5px 10px',
+              border: '1px solid #ddd',
+              borderRadius: '3px',
+              marginTop: '5px',
+            }}
+          >
+            <span className="icon icon-record" style={{ color: '#fdbc40', marginRight: '5px' }} />
+            <strong>{value.action}</strong>
+            <br />
+            Lorem ipsum dolor sit amet.
+          </div>
+        ))
+      }
     </div>
   );
 };
