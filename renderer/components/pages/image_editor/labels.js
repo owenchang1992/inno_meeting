d1d1ef2692fc593d7cb451c9edb1cb60699313b3
owenchang1992 <>
2020-09-14 17:32:48 +0800

    Add history

---
+++ renderer/components/pages/image_editor/labels.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default ({ history }) => {
+  console.log(history);
+
+  return (
+    <div>
+      <h5 className="nav-group-title">Events</h5>
+      <div className="list-group-item" style={{ padding: '5px 10px', border: '1px solid #ddd', borderRadius: '3px' }}>
+        <span className="icon icon-record" style={{ color: '#fdbc40', marginRight: '5px' }} />
+        <strong>{history.length <= 2 ? 'fdf' : history[2].action}</strong>
+        <br />
+        Lorem ipsum dolor sit amet.
+      </div>
+    </div>
+  );
+};
