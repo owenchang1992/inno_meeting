a3d70cc7ae88ffa8e0c4a6e8e2f630b6d385acd1
owenchang1992 <>
2020-09-15 17:51:34 +0800

    Hide draw image action

--- renderer/components/pages/image_editor/records.js
+++ renderer/components/pages/image_editor/records.js
@@ -2,24 +2,29 @@ import React from 'react';
 
 export default ({ history }) => (
   <>
-    <h5 className="nav-group-title">Events</h5>
+    <h5 className="nav-group-title">ROA</h5>
     {
-      history.map((value) => (
-        <div
-          className="list-group-item"
-          style={{
-            padding: '5px 10px',
-            border: '1px solid #ddd',
-            borderRadius: '3px',
-            marginTop: '5px',
-          }}
-        >
-          <span className="icon icon-record" style={{ color: '#fdbc40', marginRight: '5px' }} />
-          <strong>{value.properties.tag.name}</strong>
-          {/* <br />
-          Lorem ipsum dolor sit amet. */}
-        </div>
-      ))
+      history.map((value) => {
+        const { tag } = value.properties;
+        return value.action === 'draw-image'
+          ? null
+          : (
+            <div
+              className="list-group-item"
+              style={{
+                padding: '5px 10px',
+                border: '1px solid #ddd',
+                borderRadius: '3px',
+                marginTop: '5px',
+              }}
+            >
+              <span className="icon icon-record" style={{ color: tag.color, marginRight: '5px' }} />
+              <strong>{tag.name}</strong>
+              <br />
+              {value.action}
+            </div>
+          );
+      })
     }
   </>
 );
