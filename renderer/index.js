88e07ada4d00ae6dc1ff49f692040dd102c74e76
owenchang1992 <>
2020-09-23 14:06:26 +0800

    Fix the file name problem

--- renderer/index.js
+++ renderer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './containers/app';
+import Wrapper from './containers/wrapper';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(<Wrapper />, document.getElementById('root'));
