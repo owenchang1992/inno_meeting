1cebceec5672aaae31cfc45418fe760703aba38e
owenchang1992 <>
2020-09-24 17:23:28 +0800

    Save history to appData->media_tagger->db

--- src/models/nedb.js
+++ src/models/nedb.js
@@ -1,5 +1,20 @@
+const os = require('os');
+const Datastore = require('nedb');
+const path = require('path');
+const { app } = require('electron');
+// const fs = require('fs');
+
+const db = new Datastore({
+  filename: path.join(app.getPath('appData'), 'media_tagger/db', 'media.db'),
+  autoload: true,
+})
+
 module.exports = (() => {
   const post = (props) => {
+    db.insert(props, (err, newDoc) => {
+      console.log(newDoc);
+    })
+
     console.log('main model', props);
   }
 
