858a17f5fa99e98b33ae1c8f107e36b54cfae751
owenchang1992 <>
2020-09-24 17:33:45 +0800

    Store history with _id

--- src/models/nedb.js
+++ src/models/nedb.js
@@ -2,7 +2,6 @@ const os = require('os');
 const Datastore = require('nedb');
 const path = require('path');
 const { app } = require('electron');
-// const fs = require('fs');
 
 const db = new Datastore({
   filename: path.join(app.getPath('appData'), 'media_tagger/db', 'media.db'),
@@ -11,11 +10,9 @@ const db = new Datastore({
 
 module.exports = (() => {
   const post = (props) => {
-    db.insert(props, (err, newDoc) => {
+    db.insert(JSON.parse(props), (err, newDoc) => {
       console.log(newDoc);
     })
-
-    console.log('main model', props);
   }
 
   return {
