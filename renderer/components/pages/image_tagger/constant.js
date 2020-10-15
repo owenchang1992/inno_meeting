5c83f46647189795250cce289ecf339217444a28
owenchang1992 <>
2020-10-15 10:22:10 +0800

    Add showing tag constant

--- renderer/components/pages/image_tagger/constant.js
+++ renderer/components/pages/image_tagger/constant.js
@@ -1,3 +1,6 @@
 export const GET_TAGS_FROM_DB = 'GET_TAGS_FROM_DB';
 export const DRAW_RECTANGLE = 'DRAW_RECTANGLE';
 export const DELETE_TAG = 'DELETE_TAG';
+export const SHOW_TAG = 'SHOW_TAG';
+export const HIDE_TAG = 'HIDE_TAG';
+export const TOGGLE_TAG = 'TOGGLE_TAG';
