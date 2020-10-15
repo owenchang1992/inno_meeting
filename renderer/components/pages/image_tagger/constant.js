94e1bff4dd5e2fb681e56117575d1c3348e20558
owenchang1992 <>
2020-10-15 13:56:10 +0800

    Add comment

--- renderer/components/pages/image_tagger/constant.js
+++ renderer/components/pages/image_tagger/constant.js
@@ -1,6 +1,9 @@
+// Reducer
 export const GET_TAGS_FROM_DB = 'GET_TAGS_FROM_DB';
-export const DRAW_RECTANGLE = 'DRAW_RECTANGLE';
 export const DELETE_TAG = 'DELETE_TAG';
 export const SHOW_TAG = 'SHOW_TAG';
 export const HIDE_TAG = 'HIDE_TAG';
 export const ADD_TAG = 'ADD_TAG';
+
+// TAG TYPE
+export const DRAW_RECTANGLE = 'DRAW_RECTANGLE';
