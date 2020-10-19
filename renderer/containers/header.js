a8b86c81c20f79a538d76599a624988d776b603b
owenchang1992 <>
2020-10-19 14:42:58 +0800

    file select icon

--- renderer/containers/header.js
+++ renderer/containers/header.js
@@ -1,38 +1,49 @@
 import React from 'react';
 
-const Header = () => (
-  <div className="toolbar toolbar-header">
-    {/* <h1 className="title">Title</h1> */}
+const Header = () => {
+  const openSelectFileDialog = () => {
+    console.log('send');
+    window.api.send('toMain', 'select-file-dialog');
+  };
 
-    <div className="toolbar-actions">
-      <div className="btn-group">
+  return (
+    <div className="toolbar toolbar-header">
+      {/* <h1 className="title">Title</h1> */}
+
+      <div className="toolbar-actions">
+        <div className="btn-group">
+          {/* <button type="button" className="btn btn-default">
+            <span className="icon icon-home" />
+          </button> */}
+          <button type="button" className="btn btn-default">
+            <span className="icon icon-folder" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={openSelectFileDialog}
+          >
+            <span className="icon icon-picture" />
+          </button>
+          {/* <button type="button" className="btn btn-default">
+            <span className="icon icon-popup" />
+          </button>
+          <button type="button" className="btn btn-default">
+            <span className="icon icon-shuffle" />
+          </button> */}
+        </div>
+
         {/* <button type="button" className="btn btn-default">
-          <span className="icon icon-home" />
+          <span className="icon icon-home icon-text" />
+          Filters
         </button> */}
-        <button type="button" className="btn btn-default">
-          <span className="icon icon-folder" />
-        </button>
-        <button type="button" className="btn btn-default">
-          <span className="icon icon-picture" />
-        </button>
-        {/* <button type="button" className="btn btn-default">
-          <span className="icon icon-popup" />
-        </button>
-        <button type="button" className="btn btn-default">
-          <span className="icon icon-shuffle" />
+        {/*
+        <button type="button" className="btn btn-default btn-dropdown pull-right">
+          <span className="icon icon-cog" />
         </button> */}
       </div>
-
-      {/* <button type="button" className="btn btn-default">
-        <span className="icon icon-home icon-text" />
-        Filters
-      </button> */}
-      {/*
-      <button type="button" className="btn btn-default btn-dropdown pull-right">
-        <span className="icon icon-cog" />
-      </button> */}
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
