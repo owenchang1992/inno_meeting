a331fcfd1117ceb3e27769b7efff9306ab5e0e7c
owenchang1992 <>
2020-10-20 14:03:10 +0800

    Clean preload.js

--- src/preload.js
+++ src/preload.js
@@ -8,26 +8,22 @@ const nodeTools = require('./utils')
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
-    "api", {
-        send: (channel, data) => {
-            // whitelist channels
-            let validChannels = ["toMain", "toCurrentPage"];
-            if (validChannels.includes(channel)) {
-                ipcRenderer.send(channel, data);
-            }
-        },
-        receive: (channel, func) => {
-            let validChannels = ["fromMain", "fromCurrentPage"];
-            if (validChannels.includes(channel)) {
-                // Deliberately strip event as it includes `sender`
-                ipcRenderer.on(channel, func);
-            }
-        },
-        removeListener: (channel, func) => {
-            console.log(channel, ipcRenderer.rawListeners(channel));
-            ipcRenderer.removeAllListeners(channel, func);
-            console.log('Hi', channel, ipcRenderer.rawListeners(channel));
-        },
-        getHomeDir: nodeTools.getHomeDir
+  "api", {
+    send: (channel, data) => {
+      // whitelist channels
+      let validChannels = ["toMain", "toCurrentPage"];
+      if (validChannels.includes(channel)) {
+        ipcRenderer.send(channel, data);
+      }
+    },
+    receive: (channel, func) => {
+      let validChannels = ["fromMain", "fromCurrentPage"];
+      if (validChannels.includes(channel)) {
+        ipcRenderer.on(channel, func);
+      }
+    },
+    removeListener: (channel, func) => {
+      ipcRenderer.removeAllListeners(channel, func);
     }
+  }
 );
