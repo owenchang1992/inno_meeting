20410a441251b1715d53aedf9d68010f61031b0e
owenchang1992 <>
2020-10-23 15:46:05 +0800

    Clear menu.js

--- src/menu.js
+++ src/menu.js
@@ -6,55 +6,9 @@
 
 module.exports = function() {
   const template = [
-    // {
-    //   label: 'Edit',
-    //   submenu: [
-    //     {
-    //       label: 'Undo',
-    //       accelerator: 'CmdOrCtrl+Z',
-    //       role: 'undo'
-    //     },
-    //     {
-    //       label: 'Redo',
-    //       accelerator: 'Shift+CmdOrCtrl+Z',
-    //       role: 'redo'
-    //     },
-    //     {
-    //       type: 'separator'
-    //     },
-    //     {
-    //       label: 'Cut',
-    //       accelerator: 'CmdOrCtrl+X',
-    //       role: 'cut'
-    //     },
-    //     {
-    //       label: 'Copy',
-    //       accelerator: 'CmdOrCtrl+C',
-    //       role: 'copy'
-    //     },
-    //     {
-    //       label: 'Paste',
-    //       accelerator: 'CmdOrCtrl+V',
-    //       role: 'paste'
-    //     },
-    //     {
-    //       label: 'Select All',
-    //       accelerator: 'CmdOrCtrl+A',
-    //       role: 'selectall'
-    //     },
-    //   ]
-    // },
     {
       label: 'View',
       submenu: [
-        // {
-        //   label: 'Reload',
-        //   accelerator: 'CmdOrCtrl+R',
-        //   click: function(item, focusedWindow) {
-        //     if (focusedWindow)
-        //       focusedWindow.reload();
-        //   }
-        // },
         {
           label: 'Toggle Full Screen',
           accelerator: (function() {
@@ -68,19 +22,6 @@ module.exports = function() {
               focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
           }
         },
-        // {
-        //   label: 'Toggle Developer Tools',
-        //   accelerator: (function() {
-        //     if (process.platform === 'darwin')
-        //       return 'Alt+Command+I';
-        //     else
-        //       return 'Ctrl+Shift+I';
-        //   })(),
-        //   click: function(item, focusedWindow) {
-        //     if (focusedWindow)
-        //       focusedWindow.toggleDevTools();
-        //   }
-        // },
       ]
     },
     {
@@ -99,16 +40,6 @@ module.exports = function() {
         },
       ]
     },
-    // {
-    //   label: 'Help',
-    //   role: 'help',
-    //   submenu: [
-    //     {
-    //       label: 'Learn More',
-    //       click: function() {}
-    //     },
-    //   ]
-    // },
   ];
 
   if (process.platform === 'darwin') {
