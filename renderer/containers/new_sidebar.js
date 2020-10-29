89b046c904fbe127b7803b529a188d4f0cd7f8b2
owenchang1992 <>
2020-10-29 18:02:45 +0800

    Show selected media

--- renderer/containers/new_sidebar.js
+++ renderer/containers/new_sidebar.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const SideBarItem = ({ media }) => (
-  <li className="list-group-item active">
-    <div className="media-body">
-      <strong>{media.name}</strong>
-    </div>
-  </li>
-);
+const SideBarItem = ({ media, handleClick, focusMedia }) => {
+  const getTab = () => (
+    media.routingPath === focusMedia.routingPath
+      ? 'active'
+      : ''
+  );
+
+  return (
+    <li className={`list-group-item ${getTab()}`}>
+      <div
+        className="media-body"
+        role="button"
+        onClick={(e) => handleClick(e, media)}
+        onKeyDown={() => {}}
+        tabIndex={0}
+      >
+        <strong>{media.name}</strong>
+      </div>
+    </li>
+  );
+};
 
 const SideBar = ({ mediaList }) => {
-  console.log(mediaList);
+  const history = useHistory();
+  const [focusMedia, setFocusMedia] = useState({
+    routingPath: history.location.pathname,
+  });
+
+  const handleClick = (e, media) => {
+    setFocusMedia(media);
+    history.push(media.routingPath);
+    // if (e.target.className.indexOf('icon-cancel') !== -1) {
+    //   closeTab(tab);
+    //   if (tab.routingPath === history.location.pathname) {
+    //     history.goBack();
+    //   }
+    // } else if (history.location.pathname !== tab.routingPath) {
+    //   history.push(tab.routingPath);
+    // }
+  };
+
   const getList = () => {
     if (mediaList) {
       return mediaList.map((media) => (
-        <SideBarItem media={media} />
+        <SideBarItem
+          media={media}
+          handleClick={handleClick}
+          focusMedia={focusMedia}
+        />
       ));
     }
 
