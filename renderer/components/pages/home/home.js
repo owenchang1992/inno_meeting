46e0ff23caeddb3d4187c4bdadf89a1061ed5ac9
owenchang1992 <>
2020-11-02 13:46:15 +0800

    Add default page

--- renderer/components/pages/home/home.js
+++ renderer/components/pages/home/home.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
 export default function () {
-  return <div>My Home page</div>;
+  return (
+    <div
+      style={{
+        fontSize: '2rem',
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      Please Click
+      <span
+        className="icon icon-archive"
+        style={{
+          margin: '0 1rem',
+        }}
+      />
+      To Start
+    </div>
+  );
 }
