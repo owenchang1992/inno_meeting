2a14e33e68de97df9ca8cea9e699a2120870105f
owenchang1992 <>
2020-11-02 15:47:54 +0800

    main_pane: clean code

--- renderer/containers/main_pane.js
+++ renderer/containers/main_pane.js
@@ -5,35 +5,30 @@ import PageLoader from '../components/page_loader';
 import Home from '../components/pages/home/home';
 import SideBar from './sidebar';
 
-const main = ({ closeTab, tabs }) => {
-  console.log('new tab', tabs);
-  return (
-    <div className="pane">
-      <>
-        <Route path="/" exact component={() => <Home />} />
-        {
-          tabs.map((tab) => (
-            <Route
-              path={tab.routingPath}
-              render={(props) => (
-                <>
-                  <div className="pane-group">
-                    <SideBar tabs={tabs} closeTab={closeTab} />
-                    <div className="pane">
-                      <PageLoader
-                        {...props}
-                        properties={{ tab, closeTab }}
-                      />
-                    </div>
-                  </div>
-                </>
-              )}
-            />
-          ))
-        }
-      </>
+const MainContent = ({ tabs, closeTab }) => (
+  <div className="pane">
+    <div className="pane-group">
+      <Route path="/" exact component={() => <Home />} />
+      {
+        tabs.map((tab) => (
+          <Route
+            path={tab.routingPath}
+            render={(props) => (
+              <>
+                <SideBar tabs={tabs} closeTab={closeTab} />
+                <div className="pane">
+                  <PageLoader
+                    {...props}
+                    properties={{ tab, closeTab }}
+                  />
+                </div>
+              </>
+            )}
+          />
+        ))
+      }
     </div>
-  );
-};
+  </div>
+);
 
-export default main;
+export default MainContent;
