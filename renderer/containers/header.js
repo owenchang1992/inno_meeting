e2da588cde35374afd824b04981c619f1d593a34
owenchang1992 <>
2021-03-17 10:41:45 +0800

    Show working path

--- renderer/containers/header.js
+++ renderer/containers/header.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 // import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
 import CollectionsBookmarkIcon from '@material-ui/icons/CollectionsBookmark';
 import FolderOpenIcon from '@material-ui/icons/FolderOpen';
+import ContextStore from '../context_store';
 
 const Header = ({
   // showOpenDialog,
   exportProject,
   selectFolder,
-}) => (
-  <div
-    style={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      padding: '4px 2%',
-      alignItems: 'center',
-      backgroundColor: '#f5f5f4',
-    }}
-  >
-    <ButtonGroup aria-label="outlined button group">
-      {/* <Button
-        className="btn btn-default"
-        onClick={showOpenDialog}
-      >
-        <AddPhotoAlternateIcon className="icon" />
-      </Button> */}
-      <Button
-        className="btn btn-default"
-        onClick={selectFolder}
-      >
-        <FolderOpenIcon className="icon" />
-      </Button>
-      <Button
-        className="btn btn-default"
-        onClick={exportProject}
-      >
-        <CollectionsBookmarkIcon className="icon" />
-      </Button>
-    </ButtonGroup>
-  </div>
-);
+}) => {
+  const { workingPath } = useContext(ContextStore);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '4px 2%',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f4',
+      }}
+    >
+      <ButtonGroup aria-label="outlined button group">
+        {/* <Button
+          className="btn btn-default"
+          onClick={showOpenDialog}
+        >
+          <AddPhotoAlternateIcon className="icon" />
+        </Button> */}
+        <Button
+          className="btn btn-default"
+          onClick={selectFolder}
+        >
+          <FolderOpenIcon className="icon" />
+        </Button>
+        <Button
+          className="btn btn-default"
+          onClick={exportProject}
+        >
+          <CollectionsBookmarkIcon className="icon" />
+        </Button>
+      </ButtonGroup>
+      <div>
+        {`Working Path: ${workingPath}`}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
